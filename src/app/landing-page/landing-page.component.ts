1bdc660291721c14bf18ebeb4167f9be0fbc367b
SK <shaktikosti9999@gmail.com>
2020-10-21 00:41:20 +0530

    landing page on click link activation etc

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,13 +7,49 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit, AfterViewInit {
 
+  public currentActive = 'home';
+  public pageOffset: any = {};
+
+  @ViewChild('home', {static: false}) homeElement: ElementRef;
+  @ViewChild('aboutUs', {static: false}) aboutUsElement: ElementRef;
+  @ViewChild('howItsWork', {static: false}) howItsWorkElement: ElementRef;
+  @ViewChild('pricing', {static: false}) pricingElement: ElementRef;
+  @ViewChild('faq', {static: false}) faqElement: ElementRef;
+  @ViewChild('contactUs', {static: false}) contactUsElement: ElementRef;
   constructor() {}
 
   ngOnInit() {
-    this.loadScript();
+    // this.loadScript();
   }
 
   ngAfterViewInit(): void {
+    this.pageOffset = {
+      home: this.homeElement.nativeElement.offsetTop,
+      aboutUs: this.aboutUsElement.nativeElement.offsetTop,
+      howItsWork: this.howItsWorkElement.nativeElement.offsetTop,
+      pricing: this.pricingElement.nativeElement.offsetTop,
+      faq: this.faqElement.nativeElement.offsetTop,
+      contactUs: this.contactUsElement.nativeElement.offsetTop,
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkOffsetTop() {
+    console.log(window.pageYOffset, this.pageOffset); // this will console log our scroll position
+
+    if (window.pageYOffset <= this.pageOffset['home']) {
+      this.currentActive = 'home'
+    } else if (window.pageYOffset >= this.pageOffset['aboutUs'] && window.pageYOffset <= this.pageOffset['howItsWork']) {
+      this.currentActive = 'aboutUs'
+    } else if (window.pageYOffset >= this.pageOffset['howItsWork'] && window.pageYOffset <= this.pageOffset['pricing']) {
+      this.currentActive = 'howItsWork'
+    } else if (window.pageYOffset >= this.pageOffset['pricing'] && window.pageYOffset <= this.pageOffset['faq']) {
+      this.currentActive = 'pricing'
+    } else if (window.pageYOffset >= this.pageOffset['faq'] && window.pageYOffset <= this.pageOffset['contactUs']) {
+      this.currentActive = 'faq'
+    } else if (window.pageYOffset >= this.pageOffset['contactUs']) {
+      this.currentActive = 'contactUs'
+    }
   }
 
   public loadScript() {
